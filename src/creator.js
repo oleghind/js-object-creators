dc3c36f34f70aa00693e07da7fddcb0a7188cd28
Oleg <zkinner@gmail.com>
2015-05-22 20:54:59 -0400

    Adding more stuff

--- src/creator.js
+++ src/creator.js
@@ -1,3 +1,4 @@
+// object maker function
 function create_object(firstName, lastName) {
 	var human = {};
 	var that = this;
@@ -14,4 +15,36 @@ function create_object(firstName, lastName) {
 	
 
 	return human;
+}
+
+// constructor
+function HumanConstr(firstName, lastName) {
+	var that = this;
+	this.firstName = firstName;
+	this.lastName = lastName;
+
+	// Method will be copied to all instances, speed is compromised
+  this.getfirstName = function() {
+ 	
+		return that.firstName;
+	}
+}
+HumanConstr.prototype.getLastName = function() {
+	return this.lastName;
+}
+
+// factory
+var HumanFactory = {
+	create_object: function(firstName, lastName) {
+		var human = Object.create(this);
+		human.firstName = firstName;
+		human.lastName = lastName;
+		return human;
+	},
+	getLastName: function() {
+		return this.lastName;
+	},
+	getfirstName: function() {
+		return this.firstName;
+	}
 }
